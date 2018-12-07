1383a98a029ace7fd2cbe5fea64633fef14251b9
Shante <shante.austin19@gmail.com>
2018-12-07 11:43:02 -0500

    Created database

--- django_reddit/settings.py
+++ django_reddit/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django-extensions',
+    'rest_framework',
     'django_reddit'
 ]
 
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'django_reddit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'reddit_db',
+        'USER': 'reddit_user',
+        'PASSWORD': 'reddit',
+        'HOST': 'localhost'
+
     }
 }
 
