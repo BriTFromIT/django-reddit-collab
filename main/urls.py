5872ed0517e0399dc3a29e2fdbd88801467461d5
Shante <shante.austin19@gmail.com>
2018-12-07 16:11:10 -0500

    Added urls

--- main/urls.py
+++ main/urls.py
@@ -1,3 +1,13 @@
-urlpatterns = [
+from django.urls import path, include
+from rest_framework import routers
 
+from . import views
+
+router = routers.DefaultRouter()
+router.register('posts', views.PostView)
+router.register('comments', views.CommentViewView)
+
+
+urlpatterns = [
+    path('', include(router.urls))
 ]
